bd2b50ae1ff70c95a9640002bad6327a624ac8fd
kamidzh <kamila.dzh09@gmail.com>
2024-11-09 13:11:24 +0300

    Учла особенности timedelta.seconds

--- datacenter/models.py
+++ datacenter/models.py
@@ -47,7 +47,7 @@ def get_duration(visit):
 
 
 def format_duration(duration):
-    total_seconds = duration.seconds
+    total_seconds = int(duration.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
     seconds = total_seconds % 60
