c87d79d6972443992c5bcdfff394ff75b9b1af00
kamidzh <kamila.dzh09@gmail.com>
2024-11-30 12:58:26 +0300

    Уточнила название переменной

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,17 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.utils import timezone
-import datetime
 from django.shortcuts import render
 from datacenter.models import get_duration, format_duration
 
 
 def storage_information_view(request):
-    non_closed_visits = []
+    serialized_visits = []
     not_finished_visits = Visit.objects.filter(leaved_at=None)
     for visit in not_finished_visits:
         entered_at = timezone.localtime(visit.entered_at)
         duration = get_duration(visit)
         owner_passcard = visit.passcard
-        non_closed_visits.append(
+        serialized_visits.append(
             {
                 'who_entered': owner_passcard,
                 'entered_at': entered_at,
@@ -21,6 +19,6 @@ def storage_information_view(request):
             }
         )
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_visits,
     }
     return render(request, 'storage_information.html', context)
